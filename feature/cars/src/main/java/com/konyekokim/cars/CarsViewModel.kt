123e5332ed930d406510009becd5a39a7ebb7869
konye <konye@youverify.co>
2021-06-04 15:06:37 +0100

    add PopularCarsViewState.kt and update CarsViewModel.kt

--- feature/cars/src/main/java/com/konyekokim/cars/CarsViewModel.kt
+++ feature/cars/src/main/java/com/konyekokim/cars/CarsViewModel.kt
@@ -1,17 +1,48 @@
 package com.konyekokim.cars
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import androidx.annotation.VisibleForTesting
+import androidx.lifecycle.*
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import com.konyekokim.commons.ui.SingleLiveData
+import com.konyekokim.core.data.DataState
+import com.konyekokim.core.data.Result
 import com.konyekokim.core.data.entities.Car
+import com.konyekokim.core.network.responses.PopularCarsResponse
+import com.konyekokim.core.network.responses.PopularCarsResponseItem
+import com.konyekokim.core.repositories.CarRepository
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class CarsViewModel @Inject constructor(
-    private val pageDataSourceFactory: CarsPageDataSourceFactory
+    private val pageDataSourceFactory: CarsPageDataSourceFactory,
+    private val carRepository: CarRepository
 ): ViewModel() {
+
+    private val  _popularCarsData = MutableLiveData<List<PopularCarsResponseItem>>()
+    val popularCarsData : LiveData<List<PopularCarsResponseItem>>
+        get() = _popularCarsData
+
+    private val _popularCarsState = MutableLiveData<PopularCarsViewState>()
+    val popularCarsState : LiveData<PopularCarsViewState>
+        get() = _popularCarsState
+
+    fun getPopularCars(){
+        _popularCarsState.postValue(PopularCarsViewState(dataState = DataState.Loading))
+        viewModelScope.launch {
+            when(val response = carRepository.fetchPopularCarBrands()){
+                is Result.Success -> {
+                    val popularCars = response.data
+                    _popularCarsState.postValue(PopularCarsViewState(dataState = DataState.Success))
+                    _popularCarsData.postValue(popularCars)
+                }
+                is Result.Error -> {
+                    _popularCarsState.postValue(PopularCarsViewState(dataState = DataState.Error(
+                        ERROR_MESSAGE)))
+                }
+            }
+        }
+    }
 
     private val pagedListConfig =
         PagedList.Config.Builder()
@@ -39,5 +70,10 @@ class CarsViewModel @Inject constructor(
 
     fun retry() {
         pageDataSourceFactory.retry()
+    }
+
+    companion object {
+        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+        const val ERROR_MESSAGE = "Error occured during fetching the popular car brands"
     }
 }
