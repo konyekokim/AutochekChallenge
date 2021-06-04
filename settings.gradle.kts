944646f1ae8eac437fc3f9c2cb8de20a0b655bf2
konye <konye@youverify.co>
2021-06-04 03:52:58 +0100

    Add buildSrc implementation and necessary common utilities

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+include(
+    ":app",
+    "lib:commons",
+    "lib:navigation",
+    ":lib:resources",
+    ":lib:testcommons"
+)
+rootProject.name = "Autochek Challenge"
+
+include(":commons")
+include(":navigation")
+include(":resources")
+include(":testcommons")
