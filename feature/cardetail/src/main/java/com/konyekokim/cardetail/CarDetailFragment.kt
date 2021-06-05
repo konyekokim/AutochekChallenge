0c29dfffc82ea23c7c5a1c43bb7e1a6565501c5f
konye <konye@youverify.co>
2021-06-05 12:09:32 +0100

    add necessary functions for CarDetailFragment.kt

--- feature/cardetail/src/main/java/com/konyekokim/cardetail/CarDetailFragment.kt
+++ feature/cardetail/src/main/java/com/konyekokim/cardetail/CarDetailFragment.kt
@@ -1,11 +1,24 @@
 package com.konyekokim.cardetail
 
 import android.content.Context
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.konyekokim.cardetail.adapter.CarMediaAdapter
+import com.konyekokim.cardetail.databinding.FragmentCardetailBinding
 import com.konyekokim.cardetail.di.CarDetailModule
 import com.konyekokim.cardetail.di.DaggerCarDetailComponent
 import com.konyekokim.commons.extensions.appContext
+import com.konyekokim.commons.extensions.observe
+import com.konyekokim.commons.extensions.showSnackbar
+import com.konyekokim.core.data.DataState
 import com.konyekokim.core.di.provider.CoreComponentProvider
+import com.konyekokim.core.network.responses.CarDetailResponse
+import com.konyekokim.core.network.responses.CarMediaListItem
+import java.math.RoundingMode
 import javax.inject.Inject
 
 class CarDetailFragment : Fragment(R.layout.fragment_cardetail) {
@@ -13,9 +26,82 @@ class CarDetailFragment : Fragment(R.layout.fragment_cardetail) {
     @Inject
     lateinit var viewModel: CarDetailViewModel
 
+    private lateinit var binding: FragmentCardetailBinding
+
+    private lateinit var carMediaAdapter: CarMediaAdapter
+
+    private val args: CarDetailFragmentArgs by navArgs()
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         setUpDependencyInjection()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCardetailBinding.bind(view)
+        setUpCarMediaRecyclerView()
+        initViews()
+        observe(viewModel.carMediaListState, ::onCarMediaViewStateChanged)
+        observe(viewModel.carMediaListData, ::onCarMediaViewDataChanged)
+        observe(viewModel.carDetailState, ::onCarDetailViewStateChanged)
+        observe(viewModel.carDetailData, ::onCarDetailViewDataChanged)
+        viewModel.fetchCarDetail(args.carId)
+        viewModel.fetchCarMediaList(args.carId)
+    }
+
+    private fun initViews(){
+        binding.carNameText.text = args.carName
+        binding.backArrowImg.setOnClickListener { findNavController().popBackStack() }
+    }
+
+    private fun setUpCarMediaRecyclerView(){
+        carMediaAdapter = CarMediaAdapter(viewModel)
+        with(binding.carMediaList){
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = carMediaAdapter
+        }
+    }
+
+    private fun onCarMediaViewStateChanged(carMediaListViewState: CarMediaListViewState){
+        when(val dataState = carMediaListViewState.dataState){
+            is DataState.Error -> {
+                showSnackbar(dataState.message)
+            }
+            else -> {
+                //do nothing
+            }
+        }
+    }
+
+    private fun onCarMediaViewDataChanged(carMediaList: List<CarMediaListItem>){
+        carMediaAdapter.submitList(carMediaList)
+    }
+
+    private fun onCarDetailViewStateChanged(carDetailViewState: CarDetailViewState){
+        when(val dataState = carDetailViewState.dataState){
+            is DataState.Error -> {
+                showSnackbar(dataState.message)
+            }
+            else -> {
+                //do nothing
+            }
+        }
+    }
+
+    private fun onCarDetailViewDataChanged(carDetailResponse: CarDetailResponse){
+        binding.model.text = ("${carDetailResponse.model.make.name} ${carDetailResponse.model.name}")
+        binding.rating.text = carDetailResponse.gradeScore
+            .toBigDecimal().setScale(1, RoundingMode.UP)
+            .toDouble().toString()
+        binding.interiorColor.text = carDetailResponse.interiorColor
+        binding.exteriorColor.text = carDetailResponse.exteriorColor
+        binding.engineType.text = carDetailResponse.engineType
+        binding.year.text = carDetailResponse.year.toString()
+        binding.mileage.text = ("${carDetailResponse.mileage} ${carDetailResponse.mileageUnit}")
+        binding.city.text = carDetailResponse.city
+        binding.reason.text = carDetailResponse.reasonForSelling
+        binding.sellingCondition.text = carDetailResponse.sellingCondition
     }
 
     private fun setUpDependencyInjection(){
