3a23f418c781c239904aa582d2132c77c880f0a7
konye <konye@youverify.co>
2021-06-05 15:18:53 +0100

    add icons to project

--- app/src/main/java/com/konyekokim/autochekchallenge/MainActivity.kt
+++ app/src/main/java/com/konyekokim/autochekchallenge/MainActivity.kt
@@ -2,7 +2,9 @@ package com.konyekokim.autochekchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.konyekokim.autochekchallenge.databinding.ActivityMainBinding
+import com.konyekokim.commons.extensions.isDarkTheme
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.darkLightMode.setOnClickListener {
+            val nightMode =
+                if (isDarkTheme()) {
+                    AppCompatDelegate.MODE_NIGHT_NO
+                } else {
+                    AppCompatDelegate.MODE_NIGHT_YES
+                }
+            AppCompatDelegate.setDefaultNightMode(nightMode)
+        }
     }
 }
